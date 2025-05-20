cb52db2ff276b79fce66dae4474a2b31827fa0c9
VibhuK07 <vibhukrishnaswamy1@gmail.com>
2025-05-20 18:29:53 -0400

    Update urls.py

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
-from api.views import RegisterView
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Root endpoint
@@ -12,15 +13,14 @@ urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
     
-    # API Endpoints
+    # API Endpoints (register/ is now in api/urls.py)
     path('api/', include('api.urls')),
     
     # Authentication
-    path('api/register/', RegisterView.as_view(), name='register'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     
     # Redirect Legacy URLs
     path('accounts/profile/', RedirectView.as_view(url='/api/projects/')),
     path('accounts/login/', RedirectView.as_view(url='/api/auth/login/')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
